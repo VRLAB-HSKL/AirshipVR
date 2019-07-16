ae55aa45fd14796b588c19ca685378ec881d4f65
Anker13 <jesc0030@stud.hs-kl.de>
2019-07-16 23:45:58 +0200

    Änderungen an den Skripten Application_Quit.cs und LoadSceneAsync.cs

---
+++ AirshipDemo/Assets/Scripts/Application_Quit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HTC.UnityPlugin.Vive;
+public class Application_Quit : MonoBehaviour
+{
+   
+    [SerializeField]
+    private bool inStartScene;//Gibt an, ob sich der Benutzer gerade in der Start Szene befindet. TRUE = Ja, er befindet sich in der "Start" Szene. FALSE = Nein, er befindet sich in der "Main" Szene.
+    private AsyncOperation asyncLoad;//AsyncOperation Objekt zum späteren asynchronen Laden der "Start" Szene.
+    /// <summary>
+    /// Möchte der Benutzer die Anwendung verlassen, so muss er in der "Start" Szene nur durch den Notausgang gehen. In der "Main" Szene 
+    /// muss er die Türklinge mit dem Controller berühren.
+    /// </summary>
+    /// <param name="other"> Beschreibt das in den Collider eingetretene GameObject</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (inStartScene)//Der Benutzer befindet sich in der "Start" Szene und geht gerade durch den Türrahmen des Notausgangs.
+        {
+            if (other.gameObject.layer == 8 || other.gameObject.layer == 9)
+            {
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+            }
+        }
+        else//Der Benutzer befindet sich gerade in der "Main" Szene und möchte wieder zurück zur "Start" Anwendung.
+        {
+            if(other.GetType() == typeof(SphereCollider))//Handelt es sich um den SphereCollider eines Vive Controllers?
+            {
+                if (ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Trigger) || ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Trigger))//Wenn ja wurde ein Trigger von einem der beiden Controller getätigt.
+                {
+                    StartCoroutine(LoadAsyncScene());
+                }
+                else//Falls es zu einem unvorhergesehenen Fehler kommen sollte und ein Objekt mit einem Scene Collider durch den Box Collider der Tür fallen sollte, soll nichts passieren.
+                {
+                    return;
+                } 
+            }
+        }
+
+    }
+    IEnumerator LoadAsyncScene()//Lädt Asynchron die Szene "Start"
+    {
+        yield return asyncLoad = SceneManager.LoadSceneAsync("Start");
+    }
+}
--- AirshipDemo/Assets/Scripts/LoadSceneAsync.cs
+++ AirshipDemo/Assets/Scripts/LoadSceneAsync.cs
@@ -7,39 +7,38 @@ using TMPro;
 public class LoadSceneAsync : MonoBehaviour
 {
     [SerializeField]
-    private StartScenario radialBar;
+    private Image radialBar;//Radialbar zum Anzeigen des Ladefortschritts.
 
-    private AsyncOperation asyncLoad;
+    private AsyncOperation asyncLoad;//AsyncOperation zum späteren Laden der entsprechenden Szenen.
 
 
     [SerializeField]
-    private ParticleSystem mTrueOrFalse;
+    private ParticleSystem mTrueOrFalse;//Partikelsystem, damit der Benutzer mitbekommt, ob dieser das richtige Objekt hinzugefügt hat.
     
     [SerializeField]
-    private TextMeshProUGUI loadingLabel;
+    private TextMeshProUGUI loadingLabel;//Label unterhalb der X-Stelle.
 
-    private string sceneToLoad = "";
+    private string sceneToLoad = "";//Der String, der später angibt, welche Szene nachher geladen werden soll.
 
-    private bool isLoading = false;
+    private bool isLoading = false;//Gibt an, ob im Moment eine Szene geladen wird.
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        radialBar.OnChange(this.onRadialBarChange);
-        radialBar.onDone(this.OnRadialBarDone);
-        
+        radialBar.fillAmount = 0f;
     }
+
     /// <summary>
     /// Bei dieser Triggerabfrage, wird überprüft, ob das eintretende Objekt ein GameObject mit dem Tag "SceneGlobe" ist. 
     /// GameObjects mit dem Tag "SceneGlobe" sind zumeist eine modellierte Schneekugel mit einem Miniatur Modell zur Darstellung der auszuführenden Anwendung.
     /// Außerdem enthält der Name des GameObjects den Namen der Szene, die ausgeführt werden soll. Bitte darauf achten, dass die Szene in den Build Settings festgelegt ist.
     /// Denn nur diese speziellen GameObjects sollen im nächsten Schritt die Szene (Anhand des Namens des GameObjects) mit einer AsyncOperation gestartet werden.
     /// </summary>
-    /// <param name="other"></param>
+    /// <param name="other">Beschreibt das in den Collider eingetretene GameObject</param>
     private void OnTriggerEnter(Collider other)
     {
         
-        if(other.gameObject.tag == "SceneGlobe" && !isLoading)
+        if(other.gameObject.tag == "SceneGlobe" && !isLoading)//Es handelt sich bei dem GameObject um eine Schneekugel samt kleinem Modell. Außerdem wurde das Laden einer Szene noch nicht gestartet.
         {
             isLoading = true;
             mTrueOrFalse.startColor = Color.green;
@@ -48,11 +47,11 @@ public class LoadSceneAsync : MonoBehaviour
             loadingLabel.text = "Loading " + other.gameObject.name + " Scenario";
             StartCoroutine(LoadAsyncScence());
         }
-        else if(other.gameObject.tag == "SceneGlobe" && isLoading)
+        else if(other.gameObject.tag == "SceneGlobe" && isLoading)//Es handelt sich bei dem GameObject um eine Schneekugel, doch es wurde schon eine Szene geladen. Deswegen wird nichts ausgeführt!
         {
-
+            return;
         }
-        else
+        else//Es handelt sich um keine Schneekugel, dem Benutzer wird das durch ein rotes Partikelsystem und durch das Ändern des Labels dargestellt.
         {
             mTrueOrFalse.startColor = Color.red;
             mTrueOrFalse.Play();
@@ -60,25 +59,22 @@ public class LoadSceneAsync : MonoBehaviour
             
         }
     }
+    /// <summary>
+    /// Der Benutzer verlässt mit seinem Controller den Collider der X-Stelle.
+    /// </summary>
+    /// <param name="other"> Das GameObject, dass den Collider verlässt.</param>
     private void OnTriggerExit(Collider other)
     {
-        if (isLoading)
+        if (isLoading)//Wenn eine Szene geladen wird wird nichts ausgeführt.
             return;
-        else
+        else//Wenn nichts geladen wird, sich also auch keine Schneekugel auf der X-Stelle befindet, wird wieder der Standard Text des Labels angezeigt.
         {
             loadingLabel.text = "Place Scenario here";
             mTrueOrFalse.Stop();
         }
             
     }
-    void onRadialBarChange(float value)
-    {
-        //print("Value: " + value);
-    }
-    void OnRadialBarDone()
-    {
-        //print("Done");
-    }
+   
 
     /// <summary>
     /// Enumerator zum Laden der zuvor in OnTriggerEnter festgelegten Szene. Hierbei wird kontinuierlich die Radial Progressbar, die sich auf dem "Table" Mesh befindet
@@ -89,10 +85,10 @@ public class LoadSceneAsync : MonoBehaviour
     {
         asyncLoad = SceneManager.LoadSceneAsync(this.sceneToLoad);
       
-
+        //Solange die Szene lädt wird die RadialBar entsprechend gefüllt.
         while (!asyncLoad.isDone)
         {
-            radialBar.SetValue(Mathf.Clamp01(asyncLoad.progress / 0.9f));
+            radialBar.fillAmount = Mathf.Clamp01(asyncLoad.progress / 0.9f);
             yield return null;
         }
     }
